91160a091f6bd49af0ecae7a3b12dd97939970d8
KonuhovAND <andreykonuhov8@gmail.com>
2026-07-15 15:19:44 +0300

    basement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,9 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [msg, setMsg] = useState('Загрузка...');
-
-  useEffect(() => {
-    fetch('http://localhost:8000/api/hello/')
-      .then(res => res.json())
-      .then(data => setMsg(data.message))
-      .catch(err => setMsg('Ошибка: ' + err.message));
-  }, []);
-
   return (
     <div>
-      <h1>{msg}</h1>
+      <h1>HI</h1>
     </div>
   );
 }
